fc1ee65748cd6a48be2bb8ad7e9f7f9dbcf3fbaf
Joey Lorich <joseph@lorich.me>
2019-11-03 13:13:25 -0500

    initial demo commit

---
+++ demo-ignite2019-terraform/terraform/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  version = "=1.35.0"
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${var.prefix}-${var.name}-${var.environment}-rg"
+  location = "${var.location}"
+}
+
+resource "azurerm_app_service_plan" "default" {
+  name                = "${var.name}-plan"
+  location            = "${azurerm_resource_group.default.location}"
+  resource_group_name = "${azurerm_resource_group.default.name}"
+  kind                = "Linux"
+
+  # Reserved must be set to true for Linux App Service Plans
+  reserved = true
+
+  sku {
+    tier = "standard"
+    size = "s1"
+  }
+}
+
+resource "azurerm_app_service" "default" {
+  name                = "${var.prefix}-${var.name}-${var.environment}-app"
+  location            = "${azurerm_resource_group.default.location}"
+  resource_group_name = "${azurerm_resource_group.default.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.default.id}"
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "DOCKER|nginxdemos/hello"
+  }
+}
+
+
+output "app_name" {
+  value = "${azurerm_app_service.default.name}"
+}
---
+++ demo-ignite2019-terraform/terraform/vars.tf
@@ -0,0 +1,17 @@
+
+variable "name" {
+  default = "terraform-demo"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "location" {
+  default = "West US 2"
+}
+
+variable "prefix" {
+  default = "jl-ignite2019"
+}
+
